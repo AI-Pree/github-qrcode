10e158270094b74055c31ccd28cd790dd1983ae7
AI-Pree <sexypistolcs@gmail.com>
2021-02-22 22:11:04 +1100

    removing the helper function

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import qrcode
 
 class handler(BaseHTTPRequestHandler):
 
-    #generate a qr code based on the value provided in url path
-    def qr_code_generator(self, data, fill="black", back="white"):
-        import qrcode
+    def do_GET(self):
+        user_path = self.path
+        parse_path = dict(parse.parse_qsl(parse.urlsplit(user_path).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+
+
+        #generting qr code
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_Q,
@@ -16,24 +23,8 @@ class handler(BaseHTTPRequestHandler):
         qr.make(fit=True)
 
         image = qr.make_image(fill_color=fill, back_color=back)
-        return image
 
-    def test(self):
-        return "hey checking something"
-
-    def do_GET(self):
-        user_path = self.path
-        parse_path = dict(parse.parse_qsl(parse.urlsplit(user_path).query))
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-
-        message = test()
-        #content = open(qr_code_generator(parse_path["data"], parse_path["fill"], parse_path["background"]), 'rb')
-        # if "data" in parse_path:
-        #     message = data
-        # else:
-        #     message = "Hello, stranger!"        
+        message ="it does work"    
 
         self.wfile.write(message.encode())
         return
