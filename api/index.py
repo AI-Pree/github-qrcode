830132fde57ce4655e0eee482762218b75443168
AI-Pree <sexypistolcs@gmail.com>
2021-02-22 22:12:42 +1100

    checking erro

--- api/index.py
+++ api/index.py
@@ -11,18 +11,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
 
-
-        #generting qr code
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_Q,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        image = qr.make_image(fill_color=fill, back_color=back)
+        
 
         message ="it does work"    
 
