a92b4fedb4b50e19f0fac5eb03dea5c6e105627e
ai-pree <sexypistolcs@gmail.com>
2021-02-23 22:04:43 +1100

    checking errors

--- api/index.py
+++ api/index.py
@@ -12,7 +12,13 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
 
-        
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+
 
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
