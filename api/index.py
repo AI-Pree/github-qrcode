aeb36c6bc8739b94c343d0d4232b33d7bf4901bf
ai-pree <sexypistolcs@gmail.com>
2021-02-23 21:47:57 +1100

    re-added the qr code generator

--- api/index.py
+++ api/index.py
@@ -12,6 +12,16 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
 
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data('Some data')
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
 
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
