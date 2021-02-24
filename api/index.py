9f1d7f16132e3b8fc4a41f4aff35f38920752d28
AI-Pree <sexypistolcs@gmail.com>
2021-02-25 10:06:50 +1100

    Update index.py
    commenting code for later usage

--- api/index.py
+++ api/index.py
@@ -2,24 +2,24 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import qrcode
 import PIL
-from cowpy import cow
 import io
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         user_path = self.path
-        parse_path = dict(parse.parse_qsl(parse.urlsplit(user_path).query))
+        parse_path = dict(parse.parse_qsl(parse.urlsplit(user_path).query)) # parsing the url parameter passed by the user
         self.send_response(200)
         self.send_header('Content-type','image/png')
         self.end_headers()
-
-
+        
+        # parameters passed in the url path
         size = parse_path["size"]
         data = parse_path["data"]
         fill = parse_path["fill"]
         back = parse_path["bg"]
-
+        
+        # default QrCode configs
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -27,14 +27,13 @@ class handler(BaseHTTPRequestHandler):
             border=4,
         )
 
-        
-        
         qr.add_data(data)
         qr.make(fit=True)
-
+        
+        # generating the Qrcode image
         img = qr.make_image(fill_color=fill, back_color=back)
 
-        #converting image to byte array
+        # converting image to byte array for passing it to the client
         img_byte_array = io.BytesIO()
         img.save(img_byte_array, format="PNG")
         img_byte_array = img_byte_array.getvalue()
